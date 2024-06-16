3644ffd0a3852eb9cc2570b479c5f7ac699ca2e3
Andreas Timm <info@andreas-timm.dev>
2024-06-16 21:24:52 +0200

    feat: add checkEnv

--- tm.sh
+++ tm.sh
@@ -3,13 +3,10 @@
 # This work is licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0) License.
 # To view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
 # Author: Andreas Timm
-# Repository: https://github.com/andreas-timm/code-signature-ts
-# Version: 0.4.0
-# @sha256sum 0xe901425d12469ef7e0fdd010e2d158d5398a8a6ae7e1a79bce1f77a6b9d3237b
-# @eip191signature 0x3b08f924e994205005312fec95c30e181fb066744ca51a17d122221953cda7e37abec4184e1755385cae7837cea1a6e6f74b3d336484026338bf6d2426e9f9be1c
-
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-test -f "$SCRIPT_DIR/.env" && source "$SCRIPT_DIR/.env"
+# Repository: https://github.com/andreas-timm/tm
+# Version: 0.5.0
+# @sha256sum 0x46460d3d235dab6cd2aca7f65d3078cd103b7020a09ee81c12a4510b40cf0919
+# @eip191signature 0x0101de4a2908ce689e3242711a5addbd44f1e34b0666459dee58f46f6ea8853b19f772d924e32fe162c45f1641c9d8c428ea9c9149a2b1f113a9eba4047190341b
 
 MESSAGE=$1
 
@@ -21,6 +18,16 @@ if [ "$MESSAGE" = "-h" ]; then
     echo "  ID_AUTH"
     exit 0
 fi
+
+SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+test -f "$SCRIPT_DIR/.env" && source "$SCRIPT_DIR/.env"
+test -f "$HOME/.config/tm/env" && source "$HOME/.config/tm/env"
+
+function checkEnv() { name=$1; [ -z "${!name}" ] && name=$(pass show "tm/$name"); echo -n "${!name}"; }
+
+MESSAGE_PREFIX="$(checkEnv MESSAGE_PREFIX)"
+CHAT_ID="$(checkEnv CHAT_ID)"
+ID_AUTH="$(checkEnv ID_AUTH)"
 
 urlPrefix="https://api.telegram.org/bot${ID_AUTH}/"
 maxMessageLength=$((4096 - ${#MESSAGE_PREFIX}))
