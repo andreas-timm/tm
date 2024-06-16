063cb44485645ac84ff165fb0c327e9837c319b7
Andreas Timm <info@andreas-timm.dev>
2024-06-16 20:56:25 +0200

    feat: add license

--- tm.sh
+++ tm.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: CC-BY-4.0
+# This work is licensed under the Creative Commons Attribution 4.0 International (CC BY 4.0) License.
+# To view a copy of this license, visit https://creativecommons.org/licenses/by/4.0/
+# Author: Andreas Timm
+# Repository: https://github.com/andreas-timm/code-signature-ts
+# Version: 0.4.0
+# @sha256sum 0xe901425d12469ef7e0fdd010e2d158d5398a8a6ae7e1a79bce1f77a6b9d3237b
+# @eip191signature 0x3b08f924e994205005312fec95c30e181fb066744ca51a17d122221953cda7e37abec4184e1755385cae7837cea1a6e6f74b3d336484026338bf6d2426e9f9be1c
 
 SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 test -f "$SCRIPT_DIR/.env" && source "$SCRIPT_DIR/.env"
